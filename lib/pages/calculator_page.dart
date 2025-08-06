e12db4b8d88461a9664e258fa0fa0c8d8457b03f
Aji Suryawan <kazumovie132@gmail.com>
2025-08-06 11:13:29 +0700

    first commit

---
+++ lib/pages/calculator_page.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:latihan1_11pplg2/components/custom_button.dart';
+import 'package:latihan1_11pplg2/components/custom_textfield.dart';
+import 'package:latihan1_11pplg2/controllers/calculator_controller.dart';
+
+class CalculatorPage extends StatelessWidget {
+  CalculatorPage({super.key});
+
+  final CalculatorController calculatorController = Get.put(CalculatorController());
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Calculator"),),
+      body: Column(
+        children: [
+          CustomTextfield(controller: calculatorController.txtAngka1, label: "input angka 1", 
+          labelColor: Colors.green, pass: false,isNumber: true,),
+          CustomTextfield(controller: calculatorController.txtAngka2, label: "input angka 2", 
+          labelColor: Colors.green, pass: false,isNumber: true,),
+          Row(
+            children: [
+              CustomButton(myText: "+", myTextColor: Colors.black, onPressed: () {
+                calculatorController.tambah();
+                
+              },),
+              CustomButton(myText: "-", myTextColor: Colors.black, onPressed: () {
+                
+              },)
+            ],
+          ),
+          Row(
+            children: [
+              CustomButton(myText: "X", myTextColor: Colors.black, onPressed: () {
+                
+              },),
+              CustomButton(myText: "/", myTextColor: Colors.black, onPressed: () {
+                
+              },)
+            ],
+          ),
+          Obx((){
+            return Text("Hasil "+calculatorController.textHasil.value);
+          }),
+          CustomButton(myText: "Clear", myTextColor: Colors.green, onPressed: () {
+            
+          },)
+        ],
+      ),
+    );
+  }
+}
